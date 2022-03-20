32082cf863df88da81e29877dcaed648a2752a25
proxyphi <proxyself001@gmail.com>
2022-03-20 14:32:09 -0400

    (WIP) Writing post for es-clip-threed repo.

--- components/SiteHeader.js
+++ components/SiteHeader.js
@@ -1,4 +1,4 @@
-class BlogHeader extends HTMLElement {
+class SiteHeader extends HTMLElement {
     constructor() {
         super();
     }
@@ -7,23 +7,23 @@ class BlogHeader extends HTMLElement {
         this.innerHTML = `
         <div id="header-container">
             <h1 class="header">
-                <a href="">φ</a>
+                <a href="/index.html">φ</a>
             </h1>
 
             <div class="navigator-container">
-                <p><a href="">Projects</a></p>
+                <p><a href="/pages/projects/index.html">Projects</a></p>
                 <p>__φ__</p>
-                <p><a href="">Writing</a></p>
+                <p><a href="/pages/writing/design.html">Writing</a></p>
                 <p>__φ__</p>
-                <p><a href="">Media</a></p>
+                <p><a href="/pages/media/index.html">Media</a></p>
                 <p>__φ__</p>
                 <p><a href="">Misc</a>⠀</p>
                 <p>__φ__</p>
-                <p><a href="">About</a>⠀⠀</p>
+                <p><a href="/pages/about.html">About</a>⠀⠀</p>
             </div>
         </div>
         `;
     }
 }
 
-customElements.define('blog-header', BlogHeader);
+customElements.define('site-header', SiteHeader);
--- js/sketch.js
+++ js/sketch.js
@@ -1,33 +1,47 @@
-let x = 50;
+const CANVAS_WIDTH = 120;
+let point_color = '#fac73a';
+let timer = 7; // Time for render to stop
+
+// Starting parameters of initial circle
+let x = Math.floor(CANVAS_WIDTH / 2);
 let y = 0;
 let vx = 0;
-let vy = 24;
-const CANVAS_WIDTH = 100;
+let vy = 16;
 
-let angles;
-let prob = 0.23;
-let decay = 1.28;
-let regen = 1.3;
+// Affects branching behavior
+// Probability of branching
+let prob = 0.22;
+// Every branch decays the probability of branching by this factor 
+let decay = 0.75;
+// Ellipses which get deleted regenerate probability with this factor
+// (should probably be higher than decay, but can be slightly above to increase density
+// as rendering progresses)
+let regen = 0.73;
 
-let ellipses;
-let velocity;
+// Initialized on setup()
+let angles; // Angles which branching can occur at.
+let ellipses; // Holds array of ellipses.
+let velocity; // Velocity of first point.
+let cnv; // Canvas.
+let draw_shape_fn;
 
-let cnv;
-
+// Samples an element from an array
 function sample(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
 }
 
-function ramp(a, b) {
-    return Math.max(a, Math.min(b, frameCount / 50));
+// Slightly cheap linear interpolation between a and b.
+function ramp(a, b, speed) {
+    return Math.max(a, Math.min(b, frameCount * speed));
 }
 
-function bezier_ramp(x0, y0, x1, y1) {
-    let t = Math.max(0., Math.min(1.0, frameCount / 20));
+// Interpolation on a quadratic bezier curve
+function bezier_ramp(x0, y0, x1, y1, x2, y2, speed) {
+    let t = Math.max(0., Math.min(1.0, frameCount * speed));
 
-    let p0 = createVector(0, 0);
-    let p1 = createVector(x0, y0);
-    let p2 = createVector(x1, y1);
+    let p0 = createVector(x0, y0);
+    let p1 = createVector(x1, y1);
+    let p2 = createVector(x2, y2);
 
     p0.mult((1 - t)**2);
     p1.mult(2*(1 - t)*t);
@@ -42,24 +56,23 @@ function bezier_ramp(x0, y0, x1, y1) {
 function setup() {
     cnv = createCanvas(CANVAS_WIDTH, document.documentElement.scrollHeight);
     positionCanvas(cnv, 0, 0);
-    fill(color('#fac73a'));
-    velocity = createVector(vx, vy);
-    ellipses = [
-        [x, y, velocity]  
-    ];
 
+    fill(color(point_color));
+
+    velocity = createVector(vx, vy);
+    ellipses = [[x, y, velocity]];
     angles = [HALF_PI];
+    draw_shape_fn = sample([square, ellipse, make_triangle]);
 }
 
 function updatePos(e) {
-    if (e[0] < 0 || e[0] > 400) {
+    if (e[0] < 0 || e[0] > CANVAS_WIDTH) {
         e[2].x = -e[2].x;
     }
 
-    e[0] = e[0] + bezier_ramp(0.0, 0.2, 1.0, 1.0) * e[2].x;
-    e[1] = e[1] + bezier_ramp(0.0, 0.2, 1.0, 1.0) * e[2].y;
-    //e[0] = e[0] + e[2].x;
-    //e[1] = e[1] + e[2].y;
+    let r = bezier_ramp(0.5, 0.5, 0.8, 1.0, 1.0, 1.0, 0.03);
+    e[0] = e[0] + r * e[2].x;
+    e[1] = e[1] + r * e[2].y;
 }
 
 function branchEllipse(e) {
@@ -73,36 +86,61 @@ function branchEllipse(e) {
     return [e, new_e];
 }
 
+function make_triangle(x, y, size) {
+    let x1 = x;
+    let y1 = y;
+    let x2 = x + size;
+    let y2 = y;
+    let x3 = x;
+    let y3 = y + size;
+
+    triangle(x1, y1, x2, y2, x3, y3);
+}
+
 function draw() {
     noStroke();
     smooth();
     let new_ellipses = [];
 
     for (let e of ellipses) {
-        let size = ramp(0.0, 1.0) + 3;
-        ellipse(e[0], e[1], size, size);
+        let shape_size = ramp(0.0, 2.0, 0.01) + 4;
+        //ellipse(e[0], e[1], shape_size, shape_size);
+        //square(e[0], e[1], shape_size);
+        //make_triangle(e[0], e[1], shape_size);
+        draw_shape_fn(e[0], e[1], shape_size);
         updatePos(e);
     
+        // Deletion rules
         let is_travelling_up = e[2].y < 0;
         let is_outside_vertical_bounds = e[1] < 0 || e[1] > document.documentElement.scrollHeight;
-        let is_horizontal_and_out_of_bounds = (e[0] < 0 || e[0] > 400) && (e[2].x == vy || e[2].x == -vy);
-
+        let is_horizontal_and_out_of_bounds = (e[0] < 0 || e[0] > CANVAS_WIDTH*1.5) && (Math.abs(e[2].x) == vy);
+        // Delete point and regen some branching probability
         if (is_travelling_up || is_outside_vertical_bounds || is_horizontal_and_out_of_bounds) {
-            prob = prob * regen;
+            prob = prob / regen;
             continue;
         }
 
+        // Do branching
         if (Math.random() < prob) {
-        prob = prob / decay;
-        let eps = branchEllipse(e);
-        new_ellipses.push(eps[0]);
-        new_ellipses.push(eps[1]);
+            prob = prob * decay;
+
+            let eps = branchEllipse(e);
+            new_ellipses.push(eps[0]);
+            new_ellipses.push(eps[1]);
         } else {
-        new_ellipses.push(e);
+            new_ellipses.push(e);
         }
     }
 
     ellipses = new_ellipses;
+
+    // Timer logic
+    if (frameCount % 60 == 0 && timer > 0) {
+        timer--;
+    }
+    if (timer == 0) {
+        noLoop();
+    }
 }
 
 function positionCanvas(cnv, x, y) {
